b423b04c7da2ae394aa1aa0ef1736678c4dd4d0b
Guido Marucci Blas <guidomb@gmail.com>
2017-02-14 12:19:33 -0300

    Clarify why Mailbox must be a reference type.

--- Sources/Mailbox.swift
+++ Sources/Mailbox.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
+// This type needs to have reference semantics
+// due to how container components are rendered.
+//
+// All child components' mailboxes of a container component
+// are forwared to the container's mailbox thus the need to 
+// have a single mailbox reference. Keep in mind that a Mailbox
+// is a mutable type. Subscribers can be added anytime thus the need
+// for a reference type because any object that gets a reference to 
+// a mailbox should be able to send a message to all its subscribers
+// no matter when subscribers where added to the mailbox.
+//
+// See how the `forward` method is implemented.
 public final class Mailbox<MessageType> {
     
     fileprivate var subscribers: [(MessageType) -> ()] = []
