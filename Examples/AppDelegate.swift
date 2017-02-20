e5ac51babbbcf5e079fe3f2bd754bdc6958f2f32
Guido Marucci Blas <guidomb@gmail.com>
2017-02-19 18:12:29 -0800

    Fix issue in examples project.

--- Examples/AppDelegate.swift
+++ Examples/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  PortalViewExamples
+//  Examples
 //
 //  Created by Guido Marucci Blas on 2/18/17.
 //  Copyright © 2017 Guido Marucci Blas. All rights reserved.
@@ -11,10 +11,10 @@ import PortalView
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         
@@ -26,6 +26,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         return true
     }
-
+    
 }
 
